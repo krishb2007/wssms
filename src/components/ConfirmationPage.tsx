e1bb630e7feacd36d40ddfad7c34f3724ddb3c68
krishb2007 <krishbhandaridp@gmail.com>
2025-05-11 15:40:12 +0530

    Update ConfirmationPage.tsx

--- src/components/ConfirmationPage.tsx
+++ src/components/ConfirmationPage.tsx
@@ -45,6 +45,7 @@ const ConfirmationPage: React.FC<ConfirmationPageProps> = ({
       "tourism": "Tourism",
       "meeting": "Meeting",
       "official_visit": "Official Visit",
+      "sports": "Sports",
       "student_visit": "Student Visit"
     };
     
