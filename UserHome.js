a642ca7e4166717bdbb9ae47bf169fae8f4cb8dd
krishb2007 <krishbhandaridp@gmail.com>
2025-05-22 15:39:39 +0530

    Update UserHome.js

--- UserHome.js
+++ UserHome.js
@@ -1,1 +1,10 @@
+import React from 'react';
 
+export default function UserHome() {
+  return (
+    <div>
+      <h2>Welcome, User!</h2>
+      <p>This is the user-facing home page.</p>
+    </div>
+  );
+}
