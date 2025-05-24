c8a253858a36776fed876539c863ebf457343c54
krishb2007 <krishbhandaridp@gmail.com>
2025-05-24 22:38:06 +0530

    Update vite.config.ts

--- Netlify/vite.config.ts
+++ Netlify/vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+// These two lines are needed for __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig(({ mode }) => ({
   root: ".", // use root directory (where index.html lives)
@@ -13,19 +18,18 @@ export default defineConfig(({ mode }) => ({
 
   plugins: [
     react(),
-    // mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+  ],
 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
     },
   },
 
   build: {
     outDir: "dist",
     rollupOptions: {
-      input: path.resolve(__dirname, "index.html"), // this is important!
+      input: resolve(__dirname, "index.html"),
     },
   },
 }));
